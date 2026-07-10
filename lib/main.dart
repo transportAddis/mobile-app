f9c10a2b9b91344fcee35117f677a6a2ddf2c86f
Nazrawi <nazrawiijobs@gmail.com>
2026-07-10 05:08:28 +0300

    fix(android): bypass SSL handshake exceptions and add location permissions
    Overrode global HttpOverrides to allow debug handshakes against Render.com [16]. Added fine and coarse location permissions.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'dart:io';
 
 import 'package:mobile_app/providers/theme_provider.dart';
 import 'package:mobile_app/providers/transit_provider.dart';
 import 'package:mobile_app/screens/login_screen.dart';
 import 'package:mobile_app/theme/app_theme.dart';
 
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
+
 Future<void> main() async {
+  HttpOverrides.global = MyHttpOverrides();
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const SmartTransitApp());
 }
